932e1b294a58e6cddfcf0a108cd5b0d25d69b5e5
mascuud0715-arch <mascuud0715@gmail.com>
2026-02-23 16:59:57 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -9,7 +9,7 @@ from aiogram.fsm.storage.memory import MemoryStorage
 from aiogram.fsm.context import FSMContext
 from aiogram.fsm.state import StatesGroup, State
 
-BOT_TOKEN = os.getenv("BOT_TOKEN")
+BOT_TOKEN=os.getenv("BOT_TOKEN")
 ADMIN_ID = int(os.getenv("ADMIN_ID", "7983838654"))
 
 bot = Bot(BOT_TOKEN, parse_mode="Markdown")
